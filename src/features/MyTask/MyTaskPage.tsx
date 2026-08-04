9d93efd03db76e9c501df1af6dfda03f1ee6ff0a
Emmanuel Martínez <emmanuelmartinez@ravn.co>
2026-08-04 10:05:01 -0500

    feat: my-task table header

--- src/features/MyTask/MyTaskPage.tsx
+++ src/features/MyTask/MyTaskPage.tsx
@@ -1,9 +1,13 @@
 import './MyTaskPage.css'
+import TablesHeader from './TablesHeader'
 
 function MyTaskPage() {
   return (
     <section className="my-task">
       <h1 className="sr-only">My task</h1>
+      <div className="my-task__content">
+        <TablesHeader />
+      </div>
     </section>
   )
 }
---
+++ src/features/MyTask/TablesHeader.tsx
@@ -0,0 +1,15 @@
+import './TablesHeader.css'
+
+function TablesHeader() {
+  return (
+    <div className="my-task__header">
+      <h2 className="body body--l"># Task Name</h2>
+      <h2 className="body body--l">Task Tags</h2>
+      <h2 className="body body--l">Estimate</h2>
+      <h2 className="body body--l">Task Assign Name</h2>
+      <h2 className="body body--l">Due Date</h2>
+    </div>
+  )
+}
+
+export default TablesHeader
