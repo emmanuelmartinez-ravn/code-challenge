6408e350b641d46ebf42e4f25df4b8708df063fe
Emmanuel Martínez <emmanuelmartinez@ravn.co>
2026-08-07 11:58:10 -0500

    refactor(features): implement profile query and page

--- src/constants/User.ts
+++ src/constants/User.ts
@@ -7,3 +7,19 @@ export type User = {
 export type GetUsersResponse = {
   users: User[]
 }
+
+export type UserType = 'ADMIN' | 'CANDIDATE'
+
+export type Profile = {
+  id: string
+  avatar: string
+  fullName: string
+  email: string
+  type: UserType
+  createdAt: Date
+  updatedAt: Date
+}
+
+export type GetProfileResponse = {
+  profile: Profile
+}
